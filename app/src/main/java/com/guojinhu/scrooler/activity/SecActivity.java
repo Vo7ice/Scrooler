fd369f3ebb760faa080788d6f9d831a2365cb3a4
vo7ice <vo7ice@outlook.com>
2016-04-06 16:12:29 +0800

    day2 include toolbar touch event

--- app/src/main/java/com/guojinhu/scrooler/activity/SecActivity.java
+++ app/src/main/java/com/guojinhu/scrooler/activity/SecActivity.java
@@ -2,17 +2,35 @@ package com.guojinhu.scrooler.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
 
 import com.guojinhu.scrooler.R;
+import com.guojinhu.scrooler.widget.MultiScroller;
 
 public class SecActivity extends AppCompatActivity {
+
+    MultiScroller mScroller;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sec);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.setSupportActionBar(toolbar);
+        mScroller = (MultiScroller) findViewById(R.id.root_scroller);
+
+        if (mScroller != null) {
+            mScroller.initialize(mListener);
+        }
     }
 
+    MultiScroller.MultiScrollerListener mListener = new MultiScroller.MultiScrollerListener() {
+        @Override
+        public void onScrolledOffBottom() {
+            SecActivity.this.finish();
+        }
+    };
 
 }
